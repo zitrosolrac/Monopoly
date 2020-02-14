92c9f6500fadf9a72b8414270571e158f278639a
zitrosolrac <carlosortiz9204@gmail.com>
2020-02-13 18:46:34 -0600

    Testing push

--- src/production/Game.java
+++ src/production/Game.java
@@ -11,21 +11,21 @@ public class Game {
 	public Board gameBoard;
 	public ArrayList<Player> players;
 	public Dice dice;
-	
-	
+
+
 	public Game(){
-		
+
 		gameBoard = new Board();
 		players = new ArrayList<Player>();
 		dice = new Dice();
-		
+
 	}
-	
+
 	public int getNumberOfPlayers(Scanner sc){
-		
+
 		System.out.print("Enter how many players: ");
 		int numOfPlayers = sc.nextInt();
-		
+
 		while(numOfPlayers < 2 || numOfPlayers > 4){
 			System.out.print("Invalid number of players, enter again: ");
 			numOfPlayers = sc.nextInt();
@@ -33,9 +33,9 @@ public class Game {
 
 		return numOfPlayers;
 	}
-	
+
 	public void initializePlayerArray(){
-		
+
 		Scanner sc = new Scanner(System.in);
 		int numberOfPlayers = getNumberOfPlayers(sc);
 		sc.nextLine();
@@ -45,21 +45,21 @@ public class Game {
 			this.players.add(tempPlayer);
 			numberOfPlayers--;
 		}
-		
+
 	}
-	
+
 	public ArrayList<Player> getPlayers(){
 		return players;
 	}
-	
+
 	public void shufflePlayers(){
 		Collections.shuffle(players);
 	}
-	
+
 	public void takeTurn(){
-		
+
 		for(Player p : players){
-			System.out.println("Current Player: " + p.getToken() + "Current Position: " + p.getCurrentSpace());
+			System.out.println("Current Player: " + p.getToken() + "\nCurrent Position: " + p.getCurrentSpace());
 			int roll = dice.RollDice();
 			p.setCurrentSpace((p.getCurrentSpace() + roll) % 40);
 			int payment = gameBoard.spaces[p.getCurrentSpace()].doAction();
@@ -69,23 +69,23 @@ public class Game {
 				System.out.println(p.getMoney());
 			}
 		}
-		
+
 	}
-	
+
 	public void playGame(){
-		
+
 		this.initializePlayerArray();
 		this.shufflePlayers();
-		
+
 		while(true){
 			takeTurn();
 		}
-		
+
 	}
 	public static void main(String[] args){
-		
+
 		Game monopoly = new Game();
 		monopoly.playGame();
 	}
-	
+
 }
